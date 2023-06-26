de35ff52509880f34d5963924e06fc770d2ae202
GCBGST <123060152+Vnkyz@users.noreply.github.com>
2023-06-26 16:36:20 +0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,47 @@
+module.exports = {
+    getMember: function(message, toFind = '') {
+        toFind = toFind.toLowerCase();
+
+        let target = message.guild.members.cache.get(toFind);
+        
+        if (!target && message.mentions.members)
+            target = message.mentions.members.first();
+
+        if (!target && toFind) {
+            target = message.guild.members.find(member => {
+                return member.displayName.toLowerCase().includes(toFind) ||
+                member.user.tag.toLowerCase().includes(toFind)
+            });
+        }
+            
+        if (!target) 
+            target = message.member;
+            
+        return target;
+    },
+
+    formatDate: function(date) {
+        return new Intl.DateTimeFormat('en-US').format(date)
+    },
+
+    shuffle: function(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+    
+}
